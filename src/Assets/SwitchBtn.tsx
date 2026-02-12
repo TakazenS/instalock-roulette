3a17b39edecbf64ac456a00c43d607f75025eec9
MOINE <loris.moine@esrf.fr>
2026-02-12 11:25:36 +0100

    changes: disabled state

--- src/Assets/SwitchBtn.tsx
+++ src/Assets/SwitchBtn.tsx
@@ -3,28 +3,23 @@ import { motion } from 'framer-motion';
 import styles from './SwitchBtn.module.css';
 
 interface Props {
-  winner: string | null;
+  spinning: boolean;
   isOn: boolean;
   onToggle: () => void;
 }
 
 export default function SwitchBtn(props: Props) {
-  const { winner, isOn, onToggle } = props;
-
-  let hasWinner;
-  if (winner) {
-    hasWinner = true;
-  }
+  const { spinning, isOn, onToggle } = props;
 
   return (
     <div
-      className={`${styles.switch} ${isOn ? styles.on : styles.off} ${winner ? `${styles.disabled}` : ''}`}
-      onClick={() => !winner && onToggle()}
+      className={`${styles.switch} ${isOn ? styles.on : styles.off} ${spinning ? `${styles.disabled}` : ''}`}
+      onClick={() => !spinning && onToggle()}
     >
       <motion.button
-        className={`${styles.handle} ${winner ? `${styles.disabled}` : ''}`}
+        className={`${styles.handle} ${spinning ? `${styles.disabled}` : ''}`}
         layout
-        disabled={hasWinner}
+        disabled={spinning}
         transition={{
           type: 'spring',
           stiffness: 700,
